8cb6794374eb85593374d7652ffbfa5a3edc8923
jjilgi1127 <73861696+jjilgi1127@users.noreply.github.com>
2020-12-19 21:44:27 +0900

    비눗방울 tool 클래스 만듬

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
 let bubbleSystem;
+let tool;
 
 function setup(){
   createCanvas(600,400);
-  bubbleSystem = new BubbleSystem(createVector(width/2,50));
+
+  tool = new Tool();
+    bubbleSystem = new BubbleSystem(createVector(width/2,50));
 
 
 }
 
 function draw(){
   background(149,219,255);
+      tool.run();
     bubbleSystem.fly();
 }
 
